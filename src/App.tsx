789f055b19d7566c149522f127fb1f23ab7f1418
arash-vaziri <arash.h.vaziri@gmail.com>
2025-03-17 17:21:54 -0400

    - Button and Alerts

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,39 @@
 import ListGroup from "./components/ListGroup";
-
+import Alert from "./components/Alert";
+import Button from "./components/Button";
+import { useState } from "react";
 
 
 function App() {
 
-  const cities = ['New York', 'Paris' , 'London' , 'Tehran' , 'Thunder Bay']
+  // const cities = ['New York', 'Paris' , 'London' , 'Tehran' , 'Thunder Bay']
+
+  // const handleSelectItem = (item : string) => {
+  //   console.log('item >' , item );
+  // }
 
 
-  const handleSelectItem = (item : string) => {
-    console.log('item >' , item );
+  // return <div> <ListGroup title='Cities' items={cities} onSelectItem={handleSelectItem}/> </div>
+
+  const [ show , setShow] = useState(false);
+
+  const handleButtonClick = () => {
+    setShow(true);
   }
 
+  
 
-  return <div> <ListGroup title='Cities' items={cities} onSelectItem={handleSelectItem}/> </div>
+  return <div>
+          {
+            show &&
+            <Alert onDismissed={ () => { setShow(false) }}>
+              This is a content test <span> on more wider issue</span>.
+            </Alert>
+          } 
 
+          <Button onClick={handleButtonClick} color='danger'> Check this out </Button>
+        
+        </div>
 
 }
 
