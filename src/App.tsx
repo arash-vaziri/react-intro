d84ee3dc66e9f9cee534ab3ad829927de5c668fa
arash-vaziri <arash.h.vaziri@gmail.com>
2025-03-19 14:30:48 -0400

    - update exercises

--- src/App.tsx
+++ src/App.tsx
@@ -16,10 +16,49 @@ function App() {
   // return <div> <ListGroup title='Cities' items={cities} onSelectItem={handleSelectItem}/> </div>
 
   const [ show , setShow] = useState(false);
+  
+  const [ game , setGame ] = useState({
+    id : 1,
+    player : {
+      name : "John"
+    }
+  });
+  
+  const [ pizza , setPizza ] = useState({
+    name : 'I-Pepperony',
+    toppings : ['Mushroom']
+  });
+
+
+
 
   const handleButtonClick = () => {
     setShow(true);
   }
+
+  const handleUpdatePlayer = () => {
+    setGame({
+      ...game,
+      player : {
+        ...game.player,
+        name : 'aghdas'
+      }
+    })
+  }
+
+  const handleToppings = () => {
+    setPizza({
+      ...pizza,
+      toppings : [
+        ...pizza.toppings,
+        'mozzarella cheese'
+      ]
+    })
+  }
+
+
+  const pars = (item : any) : string  => { return JSON.stringify(item) } ;
+
 
   
 
@@ -32,6 +71,20 @@ function App() {
           } 
 
           <Button onClick={handleButtonClick} color='danger'> Check this out </Button>
+
+
+          <p>
+            { pars(game) }
+          </p>
+
+          <Button onClick={handleUpdatePlayer} color='primary'> Update player </Button>
+          
+          <p>
+            { pars(pizza) }
+          </p>
+
+          <Button onClick={handleToppings} color='primary'> Update pizza </Button>
+
         
         </div>
 
